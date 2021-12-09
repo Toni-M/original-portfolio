889c4be66c6ec91f194a47e55d6f2eceb9863504
Toni-M <toni.mrkajic@gmail.com>
2021-12-09 22:31:13 +0100

    Navbar content without styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import logo from "../logo.png";
 const Navbar = () => {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
       <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..."/></a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -14,12 +15,19 @@ const Navbar = () => {
             <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
+            <a className="nav-link" href="#">About me</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
+            <a className="nav-link" href="#">Services</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Portfolio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contact</a>
           </li>
         </ul>
+      </div>
       </div>
     </nav>
     )
